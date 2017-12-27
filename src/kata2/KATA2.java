490f73fcb1fe0e5e86d84e382b15332c52aab71e
DaniCerv <DaniCerv@192.168.1.109>
2017-12-27 02:22:27 +0100

    Kata2. Versión4. Se emplea una una clase genérica al tipo Map y se cambia el tipo int por T. Se introduce el concepto de clase genérica y tipo genérico.

--- src/kata2/KATA2.java
+++ src/kata2/KATA2.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class KATA2 {
 
     public static void main(String[] args) {
-        int  [] data   = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        Integer  [] data   = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
         Map<Integer,Integer> histogram = new HashMap<>();
         Histogram histo= new Histogram(data);
         Map<Integer,Integer> histogr= histo.getHistogram();
